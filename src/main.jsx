cf2f619335c6f72f12e263bb410ecb755a47f89a
Dagechan <takebose5@gmail.com>
2023-02-13 17:19:11 +0900

    main.jsxのstyles.css参照解除

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 //import App from '/src/components/App'
 import ModelApp from './components/model'
 import '/src/css/index.css'
-import '/src/css/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
